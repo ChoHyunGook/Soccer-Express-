c0681f354a9c09291d31c7027d40c7ba86dcc033
ChoHyunGook <whgusrnr7755@gmail.com>
2022-04-15 18:08:58 +0900

    2022-04-15 18:08

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,12 @@
 const db = require('../models/index')
 const UserSchema = db.user
 exports.signup = (req, res)=>{
+    console.log('###진행 4: 노드서버에 진입함'+JSON.stringify(req.body))
+    console.log(req.body)
+    
     new UserSchema(req.body).save(()=>{
         res.status(200).json({'result':'ok'}) 
-    })
+    }) 
 }
 
 exports.userlist = (req, res)=>{
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,5 +1,6 @@
 const { signup, userlist ,profile } = require('../controllers/user.controller');
 module.exports = x => {
+    console.log('### user.route 로 들어옴###')
     x.app.post(`${x.url}/signup`, signup)
     x.app.get(`${x.url}/list`, userlist)
     x.app.get(`${x.url}/profile/:id`, profile)
